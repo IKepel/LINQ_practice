435ff4d1e715a787a948b2c6a7cb2e9c53f87173
Kostya Popov <kepel2823@gmail.com>
2023-11-27 01:19:15 +0200

    Add project files.

---
+++ LINQ_practice/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_practice
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public int Age { get; set; }
+
+        public Person(string name, int id, int age)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+        }
+    }
+}
---
+++ LINQ_practice/Program.cs
@@ -0,0 +1,57 @@
+﻿using LINQ_practice;
+
+var array = new List<int>();
+
+for (int i = 1; i <= 100; i++)
+{
+    array.Add(i);
+}
+
+var oddNumbers = array.Where(x => x % 2 != 0).ToList();
+
+Console.WriteLine("Odd numbers: ");
+foreach (var num in oddNumbers)
+{
+    Console.Write($"{num} ");
+}
+
+var squares = array.Select(x => x * x).ToList();
+
+Console.WriteLine("\n\nThe squares of numbers: ");
+foreach (var square in squares)
+{
+    Console.Write($"{square} ");
+}
+
+var sum =  array.Sum();
+Console.WriteLine($"\n\nSum = {sum}");
+
+var people = new List<Person>();
+
+for (int i = 0; i < 20; i++)
+{
+    Person person = new Person($"Person{i + 1}", i + 1, 12 + i);
+    people.Add(person);
+}
+
+var peopleOlderThan20 = people.Where(person => person.Age > 20);
+
+Console.WriteLine("\nThe names of people over the age of 20: ");
+foreach (var person in peopleOlderThan20)
+{
+    Console.WriteLine(person.Name);
+}
+
+var filteredPeople = people
+    .Where(person => person.Age > 20)
+    .Select(person => new { Id = person.Id, Name = person.Name })
+    .OrderBy(person => person.Name)
+    .ToList();
+
+Console.WriteLine("\nFiltered and sorted people over the age of 20: ");
+foreach (var personInfo in filteredPeople)
+{
+    Console.WriteLine($"Id: {personInfo.Id}, Name: {personInfo.Name}");
+}
+
+Console.WriteLine($"\nvalue = {array.Penultimate()}");
